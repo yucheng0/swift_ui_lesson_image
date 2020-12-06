202ef417fe5af14796e8d145deda0d98106d54c5
MichaelTseng <yucheng0939@gmail.com>
2020-12-06 23:55:23 +0800

    Initial Commit

---
+++ swift_ui_lesson_image/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift_ui_lesson_image
+//
+//  Created by yuchengtseng on 2020/12/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
